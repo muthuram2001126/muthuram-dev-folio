4c2b75177fcb930d3b6d6ce5c313424079d76cc1
MuthuramMuruganandan <muthurammuruganandan@gmail.com>
2025-11-01 23:20:13 +0530

    removed base from vite.configs.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base:"/muthuram-dev-folio",
+  // base:"/muthuram-dev-folio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
